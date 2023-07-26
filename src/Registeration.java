3646b4ff2b4a9442fe857074e4be257086ec9391
Ritik Raj <ritik2358@gmail.com>
2023-07-26 12:26:42 +0530

    adding source code

---
+++ src/Registeration.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Registeration {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Username");
+		String str = sc.nextLine();
+		System.out.println("Enter your age");
+		double age = sc.nextDouble();
+		System.out.println("Enter your phone number");
+		int phn = sc.nextInt();
+		System.out.println("thanks for registering");
+	}
+}
